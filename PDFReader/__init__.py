5267769188e3e0a0093b8dde486f52dab6efe573
Ansh Mehta <ansh.mehta@somaiya.edu>
2020-07-29 11:58:31 +0530

    Third Commit

--- PDFReader/__init__.py
+++ PDFReader/__init__.py
@@ -3,6 +3,6 @@ import os
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY']=ldfjsolasfuasdfjsodfusoij4w09r8pswojufsldkfjdf9
+app.config['SECRET_KEY']='ldfjsolasfuasdfjsodfusoij4w09r8pswojufsldkfjdf9'
 
 from PDFReader import routes
